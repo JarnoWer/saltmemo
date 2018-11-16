136ee2145167f0e8f78906f7321be79401e822aa
Jarno Wermundsen <a1704467@myy.haaga-helia.fi>
2018-11-16 12:54:03 +0000

    Script makes minion to salt-master

--- minion.sh
+++ minion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update 
+sudo apt-get -y install salt-minion
 read -p "Give salt id: " saltId
-echo -e "master: 138.68.98.153\nid: $saltId"|tee tmp/minion.tmp 
+echo -e "master: 138.68.98.153\nid: $saltId"|sudo tee /etc/salt/minion
+sudo systemctl restart salt-minion.service
