2f151d0ad2cb16e5d8f16e298084d39f857433f8
Jarno Reppuli <sir.jano@netti.fi>
2018-12-06 21:48:01 +0200

    New git user script

---
+++ kitti.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Git email: " gemail
+read -p "Git name: " gname
+git config --global user.email "$gemail"
+git config --global user.name "$gname"
+git config --global credential.helper ”cache –timeout=3600”
